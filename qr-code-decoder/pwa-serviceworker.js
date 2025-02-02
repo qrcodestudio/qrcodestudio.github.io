6765065a45f19837b33fc7ca4f368acb24ba85c4
iam369 <ancodia.net@gmail.com>
2025-02-02 03:29:50 +0100

    QR Code Decoder
    - PWA fixes

--- qr-code-decoder/pwa-serviceworker.js
+++ qr-code-decoder/pwa-serviceworker.js
@@ -15,7 +15,7 @@ self.addEventListener('activate', event => {
 });
 self.addEventListener('fetch', function(event) {
 	// console.log('Fetching:', event.request.url);
-	var regex = /https:\/\/qrcodestudio.app\/qr-code-decoder\/.*/;  // var regex = /https:\/\/www.googleapis.com\/youtube\/v3\/playlistItems/;
+	var regex = /https:\/\/qrcodestudio.app\/qr\-code\-decoder\/.*/;  // var regex = /https:\/\/www.googleapis.com\/youtube\/v3\/playlistItems/;
 	if (event.request.url.match(regex)) {
 		// console.log('Worker Fetching:', event.request.url);
 		event.respondWith(
